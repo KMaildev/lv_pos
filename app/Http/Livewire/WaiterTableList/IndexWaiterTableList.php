a2c1316ddc2e8780038d716b49df43c54dcdcde9
ZinMinTun <zinmintun@ZinMinTuns-MacBook-Air.local>
2022-08-26 01:59:16 +0630

    Guests Modal

--- app/Http/Livewire/WaiterTableList/IndexWaiterTableList.php
+++ app/Http/Livewire/WaiterTableList/IndexWaiterTableList.php
@@ -10,10 +10,23 @@ class IndexWaiterTableList extends Component
 
     public $table_lists;
     public $searchTableNo;
+    public $table_list_id;
+    public $guestsModalStatus = false;
 
     public function render()
     {
         $this->table_lists =  TableList::where('table_no', 'like', '%' . $this->searchTableNo . '%')->get();
         return view('livewire.waiter-table-list.index-waiter-table-list');
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function openGuestsModal($id)
+    {
+        $this->table_list_id = $id;
+        $this->guestsModalStatus = true;
     }
 }
--- resources/views/livewire/waiter-table-list/index-waiter-table-list.blade.php
+++ resources/views/livewire/waiter-table-list/index-waiter-table-list.blade.php
@@ -4,7 +4,6 @@
         <div id="content" class="app-content p-1 ps-xl-4 pe-xl-4 pt-xl-3 pb-xl-3">
             <div class="pos pos-vertical card" id="pos">
                 <div class="pos-container card-body">
-
                     <div class="input-group">
                         <input type="text" class="form-control text-white bg-secondary" placeholder="Search Table"
                             wire:model="searchTableNo" />
@@ -20,7 +19,6 @@
                             </a>
                         </div>
                     </div>
-
                     <div class="pos-content">
                         <div class="pos">
                             <div class="pos-container">
@@ -28,7 +26,9 @@
                                     <div class="pos-content-container p-3" data-scrollbar="true" data-height="100%">
                                         <div class="row gx-3">
                                             @foreach ($table_lists as $table_list)
-                                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6 pb-3">
+                                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6 pb-3"
+                                                    data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                    wire:click="openGuestsModal({{ $table_list->id }})">
                                                     <div class="pos-checkout-table in-use card">
                                                         <a class="pos-checkout-table-container"
                                                             data-toggle="select-table">
@@ -56,4 +56,7 @@
         </div>
     </div>
 
+    @if ($guestsModalStatus)
+        @include('livewire.waiter-table-list.shared.guests_modal')
+    @endif
 </div>
